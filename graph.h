55426e10d485d8a80a9b791229a10856b6581b85
Max <96389721+TetherIO@users.noreply.github.com>
2023-06-13 22:58:06 -0700

    from pinebook

--- graph.h
+++ graph.h
@@ -1,24 +1,60 @@
 #pragma once
 
-#include "graph_node.h"
+#include <vector>
+#include <string>
+#include <map>
+#include <utility>
+
+#include "edge.h"          // Add this line
+#include "graph_node.h"    // Add this line
+
+using std::vector;
+using std::pair;
+using std::map;
+using std::string;
 
 class Graph {
-    public:
+    int width;
+    int height;
+
+public:
     // constructor and destructor
-    Graph();
+    Graph(int width, int height);
     ~Graph();
+
+    int getWidth() const;
+    int getHeight() const;
+
+
     // add node
-    void addNode(string newName);
+
     // add edge
     void addEdge(string sourceName, string destinationName, int weight);
-    //get size of graph nodes list
+
+    // get size of graph nodes list
     int getSize();
-    // shortest path, take source and return string representing the shortest paths from the source node to each destination node
-    string shortestPath(string sourceName);
+
     // minimum spanning tree
     string minimumSpanningTree();
 
-    private:
-    // some way of storing my data...
-    vector<GraphNode *> nodes;
+    // find shortest path between two nodes
+    vector<string> findShortestPath(string sourceName, string destinationName);
+
+    GraphNode* getNode(string name);
+    vector<edge> kruskalMST();
+
+    vector<edge> allEdges;
+
+    void addNode(const string &nodeName);
+
+// some way of storing my data...
+vector<GraphNode*> nodes;
+private:
+    map<string, GraphNode*> nodeMap;
+
+    // helper method to find a node given its name
+    string findRoot(std::map<string, string>& parent, string node);
+
+
+};
 };
